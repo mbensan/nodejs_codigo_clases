96fbba7d8a154c4171b7a47e36b30bfd077adb6b
Matias Bensan A <mbensan@gmail.com>
2022-02-09 21:42:07 -0300

    Se agrega ejemplo de uso de MomentJS y AXIOS para el ejercicio de NPM

--- npm_parte1/server.js
+++ npm_parte1/server.js
@@ -6,13 +6,27 @@ const uuid = require('uuid');
 
 const app = express();
 
+let usuarios;
+
 // acá realizamos los requerimientos del desafío
 
 app.get('/', (req, res) => {
   res.end('lista de usuario')
 })
 
+app.get('/momento', (req, res) => {
+  const ahora = moment().format('MMMM Do YYYY, hh:MM:SS a');
+  res.end(`Ahora es ${ahora}.`)
+})
+
+app.get('/rick',  async (req, res) => {
+  let data = await axios.get('https://rickandmortyapi.com/api/character/1')
+  res.send(`El personaje buscado se llama ${data.data.name}`);
+})
+
 app.listen(3000, () => {
   console.log(chalk.green.bgYellow('servidor funcionando en el puerto 3000'));
-  console.log('Ejemplo de identificado único', uuid.v4());
+  const id = uuid.v4();
+  console.log('Ejemplo de identificado único', id);
+  console.log('Los últimos 6 caracteres del UUID', id.substr(id.length - 6));
 })
